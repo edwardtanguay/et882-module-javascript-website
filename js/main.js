6641d11d53a3be570789f45a6a6650a346c206a6
Edward Tanguay (windows) <edward@tanguay.info>
2023-10-08 20:09:06 +0200

    finished

--- js/main.js
+++ js/main.js
@@ -1,6 +1,9 @@
+import * as tools from './tools.js';
+
 const btnInfoElem = document.querySelector('.btnInfo');
 const messageElem = document.querySelector('.message');
 
 btnInfoElem.addEventListener('click', () => {
-	messageElem.innerText = 'the button has been clicked'
+	const sentence = tools.sentencize('the button has been clicked');
+	messageElem.innerText = sentence;
 });
